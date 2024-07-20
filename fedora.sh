3b1b4820ed5ad66ceb40a1296f151022dcccf276
Heinrich Xiao <74563446+Heinrich-XIAO@users.noreply.github.com>
2024-07-20 16:36:50 -0400

    luarocks

--- fedora.sh
+++ fedora.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 
 # Fedora Specific
-sudo dnf install zsh cargo python3 python-is-python3 git gh pip alacritty flatpak gnome-tweaks vim neovim zoxide cmake
+sudo dnf install zsh cargo python3 python-is-python3 git gh pip alacritty flatpak gnome-tweaks vim neovim zoxide cmake clang luarocks
 
 if [ "$(uname -m)" = "aarch64" ]; then
     # If true, run the installation command with sudo
@@ -10,6 +10,7 @@ fi
 
 # General
 pip install spotdl
+pip install pipx
 cargo install bottom
 flatpak install flathub com.ticktick.TickTick
 flatpak install flathub dev.vencord.Vesktop
@@ -26,5 +27,13 @@ ln -s .dotfiles/.zshrc
 ln -s .dotfiles/.gitconfig
 ln -s .dotfiles/.alacritty.toml
 
+cd Music
 spotdl https://open.spotify.com/playlist/66eCVWXUFNYz5Iw3rp0aw3
 
+
+cargo install --git https://github.com/nils-emmenegger/dmoj-submit
+dmoj-submit set-config --language py:py3,cpp:cpp14 -vv
+
+
+# Install zed
+curl -f https://zed.dev/install.sh | sh
