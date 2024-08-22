0885dc1355981b5367fc1a8a1c8a52ebbc207b5f
Heinrich-XIAO <74563446+Heinrich-XIAO@users.noreply.github.com>
2024-08-22 14:39:03 +0000

    wallust

--- fedora.sh
+++ fedora.sh
@@ -1,7 +1,10 @@
 #/bin/sh
 
 # Fedora Specific
-sudo dnf install zsh cargo python3 python-is-python3 git gh pip alacritty flatpak gnome-tweaks vim neovim zoxide cmake clang luarocks fastfetch hyprland
+sudo dnf install -y zsh cargo python3 python-is-python3 git gh pip alacritty flatpak gnome-tweaks vim neovim zoxide cmake clang luarocks fastfetch hyprland strawberry waybar thunderbird fcitx5 lz4-devel NetworkManager-tui pavucontrol
+
+sudo dnf -y copr enable erikreider/SwayNotificationCenter
+sudo dnf -y install SwayNotificationCenter
 
 if [ "$(uname -m)" = "aarch64" ]; then
     # If true, run the installation command with sudo
@@ -12,6 +15,7 @@ fi
 pip install spotdl
 pip install pipx
 cargo install bottom
+cargo install wallust
 flatpak install flathub com.ticktick.TickTick
 flatpak install flathub dev.vencord.Vesktop
 flatpak install flathub com.mattjakeman.ExtensionManager
@@ -29,7 +33,7 @@ ln -s .dotfiles/.alacritty.toml
 
 cd Music
 spotdl https://open.spotify.com/playlist/66eCVWXUFNYz5Iw3rp0aw3
-
+cd ~
 
 cargo install --git https://github.com/nils-emmenegger/dmoj-submit
 dmoj-submit set-config --language py:py3,cpp:cpp14 -vv
@@ -37,3 +41,12 @@ dmoj-submit set-config --language py:py3,cpp:cpp14 -vv
 
 # Install zed
 curl -f https://zed.dev/install.sh | sh
+
+
+
+cd Downloads
+git clone https://github.com/LGFae/swww.git
+cd swww
+cargo build --release
+
+cd ~
