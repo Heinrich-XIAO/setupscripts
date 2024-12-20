8e4f7cf1ab638e0c0f4a9abafd87ffdb5d775c49
Heinrich-XIAO <74563446+Heinrich-XIAO@users.noreply.github.com>
2024-12-20 18:39:23 -0500

    cmus

--- fedora.sh
+++ fedora.sh
@@ -1,10 +1,13 @@
 #/bin/sh
 
 # Fedora Specific
-sudo dnf install -y zsh cargo python3 python-is-python3 git gh pip alacritty flatpak gnome-tweaks vim neovim zoxide cmake clang luarocks fastfetch hyprland strawberry waybar thunderbird fcitx5 lz4-devel NetworkManager-tui pavucontrol input-remapper steam
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+sudo dnf install -y zsh cargo python3 python-is-python3 git gh pip alacritty flatpak gnome-tweaks vim neovim zoxide cmake clang luarocks fastfetch hyprland strawberry waybar thunderbird fcitx5 lz4-devel NetworkManager-tui pavucontrol input-remapper steam cmus
 
 sudo dnf -y copr enable erikreider/SwayNotificationCenter
 sudo dnf -y install SwayNotificationCenter
+
+
 
 if [ "$(uname -m)" = "aarch64" ]; then
     # If true, run the installation command with sudo
