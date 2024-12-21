767050cfbb9766e6b64fe79cd01cb9f016e32d05
Heinrich-XIAO <74563446+Heinrich-XIAO@users.noreply.github.com>
2024-12-21 10:32:20 -0500

    jet brains mono

--- fedora.sh
+++ fedora.sh
@@ -2,8 +2,7 @@
 
 # Fedora Specific
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
-sudo dnf install -y zsh cargo python3 python-is-python3 git gh pip alacritty flatpak gnome-tweaks vim neovim zoxide cmake clang luarocks fastfetch hyprland strawberry waybar thunderbird fcitx5 lz4-devel NetworkManager-tui pavucontrol input-remapper steam cmus
-
+sudo dnf install -y zsh cargo python3 python-is-python3 git gh pip alacritty flatpak gnome-tweaks vim neovim zoxide cmake clang luarocks fastfetch hyprland strawberry waybar thunderbird fcitx5 lz4-devel NetworkManager-tui pavucontrol input-remapper steam cmus jetbrains-mono-fonts
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 sudo dnf -y copr enable erikreider/SwayNotificationCenter
